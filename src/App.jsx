867733b0c1d79124e054afffc46eefcf41fe5bef
Romanbel <roman.beldent@gmail.com>
2023-11-24 10:50:01 +0100

    portfolio init

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+const title = "Bonjour les gens";
+
+function App() {
+  return (
+    <>
+      <h1 id="title" className="title">
+        {title}
+      </h1>
+      <input type="text" />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa
+        quibusdam nulla rem laudantium architecto facere quaerat dignissimos
+        porro, repellendus aliquam soluta eligendi aperiam unde nisi ea quas
+        voluptates perferendis qui? Suscipit libero ducimus molestias facilis
+        architecto, est laudantium labore ullam ipsam ex autem, officia
+        reprehenderit rem illo fugiat. Exercitationem, doloremque.
+      </p>
+    </>
+  );
+}
+
+export default App;
