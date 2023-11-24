bb2b6fe73b64f7ca8c0f1e8f57c07b59cf5ffa91
Romanbel <roman.beldent@gmail.com>
2023-11-24 16:55:41 +0100

    component folder added

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,22 +1,12 @@
-const title = "Bonjour les gens";
+import Nav from "../components/Nav/Nav";
+import "./App.scss";
 
-function App() {
+const App = () => {
   return (
-    <>
-      <h1 id="title" className="title">
-        {title}
-      </h1>
-      <input type="text" />
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa
-        quibusdam nulla rem laudantium architecto facere quaerat dignissimos
-        porro, repellendus aliquam soluta eligendi aperiam unde nisi ea quas
-        voluptates perferendis qui? Suscipit libero ducimus molestias facilis
-        architecto, est laudantium labore ullam ipsam ex autem, officia
-        reprehenderit rem illo fugiat. Exercitationem, doloremque.
-      </p>
-    </>
+    <div>
+      <Nav />
+    </div>
   );
-}
+};
 
 export default App;
