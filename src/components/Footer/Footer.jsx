ab255a99e4cb0cd3699f93fd793707fbecfb2ae9
Romanbel <roman.beldent@gmail.com>
2023-11-25 01:39:05 +0100

    WIP scss

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import linkedin from "./../../assets/icons/linkedin.png";
-import twitter from "./../../assets/icons/twitter.png";
-import gmail from "./../../assets/icons/gmail.png";
+import github from "./../../assets/icons/github.png";
 import "./Footer.scss";
 
 const Footer = () => {
@@ -9,8 +8,7 @@ const Footer = () => {
         <div className="Footer">
             <div className="footer-icons">
                 <img src={linkedin} alt="" width="60" />
-                <img src={twitter} alt="" width="60" />
-                <img src={gmail} alt="" width="60" />
+                <img src={github} alt="" width="60" />
             </div>
         </div>
     );
