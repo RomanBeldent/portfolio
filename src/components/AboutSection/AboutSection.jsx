87528000e11cb13ace9dc3b68b3fb75d107df25c
Romanbel <roman.beldent@gmail.com>
2023-11-27 14:34:26 +0100

    added new font

--- src/components/AboutSection/AboutSection.jsx
+++ src/components/AboutSection/AboutSection.jsx
@@ -12,20 +12,27 @@ const AboutSection = () => {
                 </div>
                 <h3>Qui je suis</h3>
                 <div className="row">
-                    <div className="profil-pic">
-                        <img style={{
-                            width: 150,
-                            //Below lines will help to set the border radius
-                            borderBottomLeftRadius: 150,
-                            borderBottomRightRadius: 150,
-                            borderTopRightRadius: 150,
-                            borderTopLeftRadius: 150,
-                            overflow: "hidden",
-                            boxShadow: "1px 10px 30px #6477b4",
-                        }} src={profile} alt="" />
-                    </div>
+                    <img style={{
+                        width: 150,
+                        marginBottom: 300,
+                        //Below lines will help to set the border radius
+                        borderBottomLeftRadius: 150,
+                        borderBottomRightRadius: 150,
+                        borderTopRightRadius: 150,
+                        borderTopLeftRadius: 150,
+                        overflow: "hidden",
+                        boxShadow: "1px 10px 30px #6477b4",
+                    }} src={profile} alt="" />
                     <div className="resume">
                         <p>
+                            En reconversion pro (anciennement technicien son et monteur son dans le cinéma), je viens de finir une formation Développeur FullStack chez O'Clock et souhaite continuer mon apprentissage avec eux. <br />Je suis donc en recherche d'alternance "Développeur concepteur d'applications" ! <br />
+
+                            🌱 Je suis actuellement en train d'apprendre Node.js et de mettre en place un projet avec un développeur React.
+
+                            En reconversion pro (anciennement technicien son et monteur son dans le cinéma), je viens de finir une formation Développeur FullStack chez O'Clock et souhaite continuer mon apprentissage avec eux. <br />Je suis donc en recherche d'alternance "Développeur concepteur d'applications" ! <br />
+
+                            🌱 Je suis actuellement en train d'apprendre Node.js et de mettre en place un projet avec un développeur React.
+
                             En reconversion pro (anciennement technicien son et monteur son dans le cinéma), je viens de finir une formation Développeur FullStack chez O'Clock et souhaite continuer mon apprentissage avec eux. <br />Je suis donc en recherche d'alternance "Développeur concepteur d'applications" ! <br />
 
                             🌱 Je suis actuellement en train d'apprendre Node.js et de mettre en place un projet avec un développeur React.
