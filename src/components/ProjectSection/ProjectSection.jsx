7873cbfbb85066d35be4ec47a138e40e42d620c1
RomanBeldent <romanbeldent@gmail.com>
2024-02-07 07:36:26 +0100

    div melodica project

---
+++ src/components/ProjectSection/ProjectSection.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./ProjectSection.scss";
+
+const ProjectSection = () => {
+    return (
+        <section id="projects">
+            <div className="line-display">
+                        <div className="line"> </div>
+                        <h4>
+                            Mes projets
+                        </h4>
+                    </div>
+                    <h3>
+                        Mélodica
+                    </h3>
+                    <div className="projects">
+                        azdazd
+                    </div>
+        </section>
+    );
+};
+
+export default ProjectSection;
